d250c4fdb5402a23d7946976a7766525759d7fe4
anodynos <agelos.pikoulas@gmail.com>
2014-02-02 23:30:11 +0200

    browserify version '3.24.10'

--- cluster.js
+++ cluster.js
@@ -1,14 +1,14 @@
-/** **nodejs 'cluster' core module.**
+/** 'cluster' nodejs core module browserify-ied with `--standalone cluster`. Should support all module systems (commonjs, AMD & `window.cluster`) - check browserify docs.
 
 From [node2web](http://github.com/anodynos/node2web) collection,
-via [browserify-CDN](http://wzrd.in/),
-exposed as 'cluster' to [bower](http://bower.io) for *browser* usage.
+should/will be exposed as 'cluster' to [bower](http://bower.io) for *browser* usage.
 
-Latest [browserify-CDN](http://wzrd.in/) reported **version: 'v0.10.12'** 
+browserify version: '3.24.10', build date 'Sun Feb 02 2014 23:30:11 GMT+0200 (EET)' 
 **/
-!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.cluster=e():"undefined"!=typeof global?global.cluster=e():"undefined"!=typeof self&&(self.cluster=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.cluster=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
-},{}]},{},[1])
-(1)
+},{}],2:[function(_dereq_,module,exports){
+module.exports = _dereq_('cluster');
+},{"cluster":1}]},{},[2])
+(2)
 });
-;
--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
-**nodejs 'cluster' core module.**
+'cluster' nodejs core module browserify-ied with `--standalone cluster`. Should support all module systems (commonjs, AMD & `window.cluster`) - check browserify docs.
 
 From [node2web](http://github.com/anodynos/node2web) collection,
-via [browserify-CDN](http://wzrd.in/),
-exposed as 'cluster' to [bower](http://bower.io) for *browser* usage.
+should/will be exposed as 'cluster' to [bower](http://bower.io) for *browser* usage.
 
-Latest [browserify-CDN](http://wzrd.in/) reported **version: 'v0.10.12'**
+browserify version: '3.24.10', build date 'Sun Feb 02 2014 23:30:11 GMT+0200 (EET)'
